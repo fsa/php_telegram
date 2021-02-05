316dbd80332145b352f082727b8199abd6fb6750
Sergey Fedotov <fsa@tavda.info>
2021-02-05 15:34:12 +0700

    WebHook теперь не зависит от внешних классов.

--- classes/telegram/webhook.php
+++ classes/telegram/webhook.php
@@ -6,45 +6,72 @@
 
 namespace Telegram;
 
-use Logger,
-    Settings;
-
 class Webhook {
 
+    private static $logger;
     private static $chat_id;
-    private $json;
+    private static $abuse;
+    private static $json;
+    private static Entity\Update $update;
 
-    public function __construct() {
-        $this->json=file_get_contents('php://input');
+    public static function setLogger($logger) {
+        self::$logger=$logger;
     }
 
-    public function getRequest() {
-        return $this->json;
+    public static function getUpdate(string $abuse=null): Entity\Update {
+        self::$json=file_get_contents('php://input');
+        if(isset($abuse)) {
+            self::$abuse=$abuse;
+            self::setExceptionHandler($abuse);
+        }
+        self::$update=new Entity\Update(json_decode(self::$json, true));
+        return self::$update;
     }
 
-    public function getUpdate(): Entity\Update {
-        return new Entity\Update(json_decode($this->json, true));
+    public static function getUpdateEntity(): Entity\Update {
+        return self::$update;
     }
 
-    public function setExceptionHandler($chat_id) {
+    public static function getRequestJson() {
+        return self::$json;
+    }
+
+    public static function logRequest() {
+        if(isset(self::$logger)) {
+            self::$logger->debug(self::$json);
+        }
+    }
+
+    public static function setExceptionHandler($chat_id) {
         self::$chat_id=$chat_id;
         set_exception_handler([self::class, 'Exception']);
     }
 
     public static function Exception($ex) {
-        switch (get_class($ex)) {
-            case 'AppException':
+        $class=get_class($ex);
+        if($class=='AppException') {
+            if(isset(self::$chat_id)) {
                 $message=new Api\SendMessage(self::$chat_id, $ex->getMessage(), 'HTML');
                 $message->webhookReplyJson();
-                exit;
-            default:
-                $admin=new Api\SendMessage(Settings::get('telegram')['abuse'], "Произошла ошибка у пользователя ".self::$chat_id."\n".$ex, 'HTML');
-                $admin->httpPost();
-                Logger::error('telegram', $ex);
-                $message=new Api\SendMessage(self::$chat_id, 'Программная ошибка при выполнении запроса.', 'HTML');
-                $message->webhookReplyJson();
+            }
+            exit;
         }
-        exit;
+        if(isset(self::$logger)) {
+            self::$logger->error($ex);
+        }
+        if(isset(self::$abuse)) {
+            if(isset(self::$chat_id)) {
+                $admin=new Api\SendMessage(self::$abuse, "Произошла ошибка у пользователя ".self::$chat_id."\n".$ex, 'HTML');
+            } else {
+                $admin=new Api\SendMessage(self::$abuse, "Произошла ошибка\n".$ex, 'HTML');
+            }
+            $admin->httpPost();
+        }
+        if(isset(self::$chat_id)) {
+            $message=new Api\SendMessage(self::$chat_id, 'Что-то пошло не так.', 'HTML');
+            $message->webhookReplyJson();
+            exit;
+        }
     }
 
 }
